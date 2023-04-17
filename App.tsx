cd833e73faac31ce20ef451df4ab2c9682599de9
GERMAN <german@MacBook-Pro-de-GERMAN.local>
2023-04-17 16:00:18 -0500

    init(navigation) Inint navigation project.

---
+++ App.tsx
@@ -0,0 +1,13 @@
+import { NavigationContainer } from '@react-navigation/native'
+import React from 'react'
+import { Text } from 'react-native'
+
+const App = () => {
+  return (
+    <NavigationContainer>
+      <Text>App Screen</Text>
+    </NavigationContainer>
+  )
+}
+
+export default App
